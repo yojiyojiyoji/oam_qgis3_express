ada1aa7c79efdf33dcf0fe33857aa349c7d6bffb
yojiyojiyoji <yoji.salut@gmail.com>
2019-02-26 11:21:19 -0500

    Adjusted the position of image browser

--- gui/img_browser.py
+++ gui/img_browser.py
@@ -4,12 +4,18 @@ import os
 from PyQt5 import uic
 from PyQt5 import QtWidgets
 
+from PyQt5.QtCore import Qt
+
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'ui/img_browser.ui'))
 
 
 class ImgBrowser(QtWidgets.QDialog, FORM_CLASS):
+
+    POSITION_WINDOW_FROM_RIGHT = 50
+    POSITION_WINDOW_FROM_TOP = 100
+
     def __init__(self, iface, parent=None):
         """Constructor."""
         #super(OAMQGIS3Dialog, self).__init__(parent)
@@ -19,4 +25,16 @@ class ImgBrowser(QtWidgets.QDialog, FORM_CLASS):
         # self.<objectname>, and you can use autoconnect slots - see
         # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
+        self.iface = iface
         self.setupUi(self)
+
+        self.setWindowFlags(Qt.WindowCloseButtonHint |
+                            Qt.WindowMinimizeButtonHint)
+
+        screenShape = QtWidgets.QDesktopWidget().screenGeometry()
+        width, height = screenShape.width(), screenShape.height()
+        winW, winH = (self.frameGeometry().width(),
+                      self.frameGeometry().height())
+        left = width - (winW + ImgBrowser.POSITION_WINDOW_FROM_RIGHT)
+        top = ImgBrowser.POSITION_WINDOW_FROM_TOP
+        self.move(left, top)
