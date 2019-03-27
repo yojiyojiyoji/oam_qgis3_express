0f29499914b7bae034d4131d90020e69e5e74de0
yojiyojiyoji <yoji.salut@gmail.com>
2019-03-27 11:28:09 -0400

    Test version for ssl/certifi library in nominatim_search.

--- module/module_geocoding.py
+++ module/module_geocoding.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-# import certifi
-# import ssl
-# import geopy.geocoders
+import certifi
+import ssl
+import geopy.geocoders
 from geopy.geocoders import Nominatim
 
 def nominatim_search(addressIn):
-    # ctx = ssl.create_default_context(cafile=certifi.where())
-    # geopy.geocoders.options.default_ssl_context = ctx
-    # geolocator = Nominatim()
-    geolocator = Nominatim(scheme='http')
+    ctx = ssl.create_default_context(cafile=certifi.where())
+    geopy.geocoders.options.default_ssl_context = ctx
+    geolocator = Nominatim()
+    # geolocator = Nominatim(scheme='http')
     location = geolocator.geocode(addressIn)
     if location is not None:
         # print('boundingbox: {}'.format(location.raw['boundingbox']))
